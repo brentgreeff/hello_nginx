3860ef91eb875fe97520d9659989ba1a31c1d081
Brent <brentgreeff@gmail.com>
2020-09-06 12:03:44 +0200

    Nginx in a box.

---
+++ doc.sh
@@ -0,0 +1,47 @@
+alias doc="docker"
+alias pose="docker-compose"
+
+up() {
+  set -x
+  pose up --force-recreate "$@"
+  set +x
+}
+
+build() {
+  set -x
+  pose build "$@"
+  set +x
+}
+
+b() {
+  set -x
+  docker build \
+         --build-arg USER_ID=$(id -u) \
+         --build-arg GROUP_ID=$(id -g) \
+         -t mathapp .
+  set +x
+}
+
+dash() {
+  set -x
+  pose exec api bash
+  set +x
+}
+
+down() {
+  set -x
+  pose down "$@"
+  set +x
+}
+
+logs() {
+  set -x
+  pose logs -f "$@"
+  set +x
+}
+
+vol() {
+  set -x
+  doc volume inspect $1
+  set +x
+}
